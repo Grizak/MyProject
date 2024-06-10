d52373a0f6254633b1c678ccb02f2b54cea69eb2
Isak Grönlund <135023164+Grizak@users.noreply.github.com>
2024-06-10 11:27:47 +0000

    fjeods

--- Web/JS/Main.js
+++ Web/JS/Main.js
@@ -7,8 +7,9 @@ let fare = {
       this.balance += pay;
       alert('Your balance passed under 0');
       alert("Your balance can't go under 0");
+      document.getElementById('bal').textContent = fare.balance;
     } else {
-      console.log(this.balance);
+      document.getElementById('bal').textContent = fare.balance;
     }
   },
   add: function() {
@@ -17,38 +18,44 @@ let fare = {
       alert('The value you have inserted is not an integer!');
     } else {
       this.balance += addBalance;
-      console.log(this.balance);
+      document.getElementById('bal').textContent = fare.balance;
     }
   },
   reset: function() {
     this.balance = 0;
-    console.log(this.balance);
+    document.getElementById('bal').textContent = fare.balance;
   },
   zone: {
     enter: 0,
     exit: 0,
     enterd: false,
-    enterzone,
-    exitzone,
+    enterzone: 0, // Defined enterzone
+    exitzone: 0,  // Defined exitzone
     enterfunc: function(fz) {
       this.enter = fz;
-      console.log("You have enter'd fare zone " + fz)
-      this.enterd = true
+      console.log("You have enter'd fare zone " + fz);
+      this.enterd = true;
+      document.getElementById('bal').textContent = fare.balance;
     },
     exitfunc: function(fz) {
       this.exit = fz;
-      console.log("You have exited fare zone " + fz)
-      fare.calc(this.enter, this.exit); // Correctly reference the outer 'fare' object
-      this.enterd = false
+      console.log("You have exited fare zone " + fz);
+      fare.calc(fare.zone.enter, fare.zone.exit); // Correctly reference the outer 'fare' object
+      this.enterd = false;
+      document.getElementById('bal').textContent = fare.balance;
     },
     toggleenterexit: function (fz) {
       if (!this.enterd) {
-        this.enterzone = fz
-        this.enterfunc(this.enterzone)
-      } else if (this.enterd) {
-        this.exitzone = fz
-        this.exitfunc(this.exitzone)
+        this.enterzone = fz;
+        this.enterfunc(this.enterzone);
+        document.getElementById('bal').textContent = fare.balance;
+      } else {
+        this.exitzone = fz;
+        this.exitfunc(this.exitzone);
+        document.getElementById('bal').textContent = fare.balance;
       }
     }
   }
 };
+
+document.getElementById('bal').textContent = fare.balance;
